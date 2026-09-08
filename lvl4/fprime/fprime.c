551dafc595f4d9a42bd7cc3d038197cb2e316fe0
Claudia Marino <cmarino@c2r2s7.42roma.it>
2026-09-08 16:42:55 +0200

    lvl4last

---
+++ lvl4/fprime/fprime.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void	fprime(int nbr)
+{
+	int div;
+
+	div = 1;
+	if (nbr < 0)
+		return ;
+	if (nbr == 1)
+		printf("1");
+	while (nbr > div++)
+	{
+		if (nbr % div == 0)
+		{
+			printf("%d", div);
+			if (nbr == div)
+				break ;
+			printf("*");
+			nbr /= div;
+			div = 1;
+		}
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+		fprime(atoi(argv[1]));
+	printf("\n");
+	return (0);
+}
